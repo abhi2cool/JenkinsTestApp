384f8763a30a943081b0ddb508bbbdb86019ed54
abhi2cool <abhikghosh41@gmail.com>
2018-10-05 00:55:38 +0530

    ghjghj

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace UnitTests
         [TestMethod]
         public void TestMethod1()
         {
-            String test6 = null;
+            String test6 = "jkljlk";
             int n = test6.Length;
         }
     }
